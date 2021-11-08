993292b7d1500f759669b5f822d49703649d94ca
S94481rth <93481116+S94481rth@users.noreply.github.com>
2021-11-08 20:58:16 +0530

    Create binarysearch.asm

---
+++ binarysearch.asm
@@ -0,0 +1,60 @@
+.MODEL SMALL
+DISPLAY MACRO MSG
+        LEA DX,MSG
+        MOV AH,09H
+        INT 21H
+ENDM
+.DATA
+LIST DB 01H,03H,04H,05H,06H,09H
+NUMBER EQU ($-LIST)
+KEY DB 08H
+MSG1 DB 0DH,0AH,"ELEMENT FOUND IN LIST ..$"
+MSG2 DB 0DH,0AH,"SEARCH FAILED !! ELEMENT NOT FOUND IN LIST$"
+
+.CODE
+START : MOV AX,@DATA
+        MOV DS,AX
+        MOV CH,NUMBER-1
+        MOV CL,00H
+AGAIN : LEA SI,LIST
+        XOR AX,AX
+        CMP CL,CH
+        JE NEXT
+        JNC FAILED
+NEXT : MOV AL,CL
+       ADD AL,CH
+       SHR AL,01H
+       MOV BL,AL
+       XOR AH,AH
+       MOV BP,AX
+       MOV AL,DS:[BP][SI]
+       CMP AL,KEY
+       JE SUCCESS
+       JC INCLOW
+       MOV CH,BL
+       DEC CH
+       JMP AGAIN
+INCLOW: MOV CL,BL
+        INC CL
+        JMP AGAIN
+SUCCESS:MOV AL,KEY
+        ADD AL,30H
+        MOV DL,AL
+        MOV AH,02H
+        INT 21H
+        DISPLAY MSG1
+        JMP FINAL
+FAILED: MOV AL,KEY
+        ADD AL,30H
+        MOV DL,AL
+        MOV AH,02H
+        INT 21H
+        DISPLAY MSG2
+FINAL : 
+        MOV AH,01H
+        INT 21H
+
+        MOV AH,4CH
+        INT 21H
+END START
+
